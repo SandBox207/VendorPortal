8b50a44c54d6d28a29d9090ec64eb2ef90885401
sand box <boxsand207@gmail.com>
2024-10-03 17:20:38 +0000

    second commit

---
+++ ADNOC-ADD-VP/app/vendorportal/dist/controller/VendorPortalMain.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(n){"use strict";return n.extend("ae.add.adnoc.vendorportal.controller.VendorPortalMain",{onInit:function(){}})});
+//# sourceMappingURL=VendorPortalMain.controller.js.map
